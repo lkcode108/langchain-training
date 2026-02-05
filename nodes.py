8f7356f1b8baf52f3d8274369596aca8fdae61ba
SINGH <lsingh38@dxc.com>
2026-02-05 06:55:01 +0530

    nodes

---
+++ nodes.py
@@ -0,0 +1,20 @@
+from dotenv import load_dotenv
+from langchain_classic.evaluation.comparison.prompt import SYSTEM_MESSAGE
+from langgraph.graph import MessagesState
+from langgraph.prebuilt import ToolNode
+from react import llm,tools
+
+load_dotenv()
+
+SYSTEM_MESSAGE ="""
+You are a helpful assistant that can use tools to answer question
+"""
+
+def run_agent_reasoning(state:MessagesState)->MessagesState:
+    """
+    Run the agent reasoning node
+    """
+    response = llm.invoke([{"role": "system", "content": SYSTEM_MESSAGE},*state["messages"]])
+    return {"messages": [response]}
+
+tool_node = ToolNode(tools)
