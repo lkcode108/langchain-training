c22f01159f28a99faff3f4a3a5ff50bdea012a86
SINGH <lsingh38@dxc.com>
2026-02-15 06:17:17 +0530

    project-setup

---
+++ main.py
@@ -0,0 +1,10 @@
+import asyncio
+
+from dotenv import load_dotenv
+load_dotenv()
+
+async def main():
+    print("Hello from langchain-mcp-server-training!")
+
+if __name__ == "__main__":
+    asyncio.run(main())
